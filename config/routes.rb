baefe82c6bddef57feacf57fff32379e79ffab13
kevin collier <collier.kevin.m@gmail.com>
2023-07-17 18:01:38 +0900

    updated root to pages-home to listings-index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'listings#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :listings, only: %i[index new create destroy]
+  resources :listings, only: %i[index show new create destroy]
 end
