55a1b11e7513e69556985ea35398fb241831935e
Juri Kato <dance.luv48@gmail.com>
2023-07-22 15:49:39 +0900

    pull from this master once merged

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index" i lik dumplings
 
   resources :listings, only: %i[index show new create destroy] do
     resources :bookings, only: %i[new create]
