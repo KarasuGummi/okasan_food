c07f97a66bca7d76e0dfa5e59f8ef748a75f63a7
kevin collier <collier.kevin.m@gmail.com>
2023-07-26 21:11:33 +0900

    added photo to user model with active record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings, dependent: :delete_all
   has_many :listings, dependent: :delete_all
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
