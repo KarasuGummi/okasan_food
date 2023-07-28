241ce86a9154512077ee150b3619b396ca563426
Juri Kato <dance.luv48@gmail.com>
2023-07-28 21:00:05 +0900

    fixed the size of avater in Homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_002145) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_125729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_002145) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "listing_id"
+    t.index ["listing_id"], name: "index_reviews_on_listing_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,10 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_002145) do
     t.boolean "momma"
     t.string "address"
     t.string "password"
-    t.binary "avatar"
-    t.string "fav_cuisine"
-    t.string "occupation"
-    t.string "language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -92,4 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_002145) do
   add_foreign_key "bookings", "listings"
   add_foreign_key "bookings", "users"
   add_foreign_key "listings", "users"
+  add_foreign_key "reviews", "listings"
+  add_foreign_key "reviews", "users"
 end
