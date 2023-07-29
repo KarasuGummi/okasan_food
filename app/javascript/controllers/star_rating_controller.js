854a1ff98033ff759675c4468c9d67e0ae979b17
Curtis Lu <karasugummi@gmail.com>
2023-07-29 11:09:14 +0900

    commented out a line that might be causing heroku problems

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import StarRating from "star-rating.js"
-import { Controller } from "stimulus";
+// import { Controller } from "stimulus";
 
 export default class extends Controller {
   connect() {
